b8d5dc3f6c6a42c7a3c2e497ebe41459180573a8
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-07-19 22:43:33 -0300

    Configure Firebase

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,21 +1,82 @@
+import { useState, useEffect } from 'react';
 import { useRoute } from '@react-navigation/native';
-import { Text, VStack } from 'native-base';
+import { HStack, Text, VStack, useTheme } from 'native-base';
+import firestore from '@react-native-firebase/firestore';
+
 import { Header } from '../components/Header';
+import { OrderProps } from '../components/Order';
+import { Loading } from '../components/Loading';
+
+import { dateFormat } from '../utils/firestoreDateFormat';
+import { OrderFireStoreDTO } from '../DTOs/OrderFireStoreDTO';
+import { CircleWavyCheck, Hourglass } from 'phosphor-react-native';
 
 type RouteParams = {
   orderId: string;
 }
 
+type OrderDetails = OrderProps & {
+  description: string;
+  solution: string;
+  closed: string;
+}
+
 export function Details() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [solution, setSolution] = useState('');
+  const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
+
+  const { colors } = useTheme();
+
   const route = useRoute();
   const { orderId } = route.params as RouteParams;
+
+  useEffect(() => {
+    firestore()
+    .collection<OrderFireStoreDTO>('orders')
+    .doc(orderId)
+    .get()
+    .then((doc) => {
+      const { patrimony, description, status, created_at, closed_at, solution } = doc.data;
+      const closed = closed_at ? dateFormat(closed_at) : null;
+
+      setOrder({
+        id: doc.id,
+        patrimony,
+        description,
+        status,
+        solution,
+        when: dateFormat(created_at),
+        closed,
+      });
+      setIsLoading(false);
+    })
+  },[])
+
+  if(isLoading) {
+    return <Loading />
+  }
+
   return (
     <VStack flex={1} bg="gray.700">
 
       <Header title="solicitação" />
-      <Text color="white">
-        {orderId}
-      </Text>
+      <HStack bg="gray.500" justifyContent="center" p={4}>
+        {
+          order.status === 'closed'
+            ? <CircleWavyCheck size={22} color={colors.green[300]} />
+            : <Hourglass size={22} color={colors.secondary[700]} />
+        }
+
+        <Text
+          fontSize="sm"
+          color={order.status === 'closed' ? colors.green[300] : colors.secondary[700]}
+          ml={2}
+          textTransform="uppercase"
+        >
+          
+        </Text>
+      </HStack>
 
     </VStack>
   );
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth'
+
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native'
 
@@ -7,10 +10,35 @@ import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn() {
-  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setpassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert('Entrar', 'Informe e-mail e senha.');
+    }
+    setIsLoading(true)
+
+    auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => {
+      setIsLoading(false);
+
+      if (error.code === 'auth/invalid-email') {
+        return Alert.alert('Entrar', 'E-mail inválido.');
+      }
+      if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password') {
+        return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+      }
+      
+      return Alert.alert('Entrar', 'Não foi possível acessar.')
+
+    })
+
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -25,7 +53,7 @@ export function SignIn() {
         mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-        onChangeText={setName}
+        onChangeText={setEmail}
       />
 
       <Input
@@ -36,7 +64,7 @@ export function SignIn() {
         onChangeText={setpassword}
       />
 
-      <Button title="Entrar" w="full"/>
+      <Button title="Entrar" w="full" onPress={handleSignIn} isLoading={isLoading} />
 
     </VStack>
   )
